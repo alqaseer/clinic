e111695387f0bacb89f04ca608c3da73cc739abe
unknown <dr.alqaseer@gmail.com>
2025-05-15 20:06:12 +0300

    fixing raw in template2222

--- core/views.py
+++ core/views.py
@@ -2193,14 +2193,3 @@ def system_referrals_stats(request):
     
     return render(request, 'system_referrals_stats.html', context)
 
-@register.filter
-def month_name(month_number):
-    """
-    Template filter that returns the name of a month from its number.
-    Usage: {{ 5|month_name }} will output "May"
-    """
-    import calendar
-    try:
-        return calendar.month_name[int(month_number)]
-    except (IndexError, ValueError):
-        return ""
